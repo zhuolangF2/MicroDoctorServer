e9deb54141ac6671b00d9f6aa637ed97528925e4
wunaifu <1278022159@qq.com>
2016-11-07 22:01:56 +0800

    删除一句引入

--- src/com/zhuolang/action/AppointmentAction.java
+++ src/com/zhuolang/action/AppointmentAction.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.opensymphony.xwork2.ActionSupport;
-import com.zhuolang.dto.AppointmentDto;
+//import com.zhuolang.dto.AppointmentDto;
 import com.zhuolang.dto.DoctorDto;
 import com.zhuolang.model.Appointment;
 import com.zhuolang.model.User;
